4490b1d4537c1c6453fbdf61d1aac67f337e14db
Kajal lathiya <kajalbodarya77@gmail.com>
2022-11-07 23:29:52 +0530

    implement count unique data

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const daftPunkDivNode = document.getElementById('daftPunkDiv');
 const metallicaDivNode = document.getElementById('metallicaDiv');
 const modalBodyNode = document.querySelector('.modal-body');
 const btnsongs = document.getElementById('btnsongs');
+const btnCountUnique = document.getElementById('btnCountUnique');
 let songlist;
+
+btnCountUnique.addEventListener('click', () => {
+    const map = [];
+    for (let value of songlist) {
+        if (map.indexOf(value.artist.name) === -1) {
+            map.push(value.artist.name);
+        }
+    }
+    alert(`Total ${map.length} unique data`)
+
+})
 
 btnsongs.addEventListener('click', () => {
     songlist.map((d, i) => {
@@ -36,12 +48,19 @@ const searchAPI = (name) => {
         .then(response => {
             firstDivNode.innerHTML = '';
             const { data } = response;
+            console.log('data:', data);
             songlist = data;
             data.map((obj, i) => {
                 return firstDivNode.innerHTML +=
                     `<div class="col-sm-6 col-md-4 col-xl-3 px-1 mb-3 mb-sm-2 mb-md-0 mb-xl-2">
                         <div class="card" style="width: 18rem">
-                            <img src=${obj.artist.picture} class="card-img-top" alt="..." />
+                            <img src=${obj.artist.picture_xl}
+                            srcset="${obj.artist.picture_small} 320w,
+                                    ${obj.artist.picture_medium} 768w,
+                                    ${obj.artist.picture_big} 1024w"
+                                    sizes="(max-width: 320px) 280px,
+                                    (max-width: 768px) 720px, 1024px"
+                             class="card-img-top" alt="..." />
                             <div class="card-body">
                             <h5 class="card-title">${obj.title_short}</h5>
                             <p class="card-text">${obj.title}</p>
