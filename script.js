c8cb9842d7da969b366319c8ac29a91fda5b0637
Kajal lathiya <kajalbodarya77@gmail.com>
2022-11-07 20:21:59 +0530

    integrate search API and retrive data

---
+++ script.js
@@ -0,0 +1,57 @@
+const firstDivNode = document.getElementById('main-content');
+const pinkdivNode = document.getElementById('pinkdiv');
+const daftPunkDivNode = document.getElementById('daftPunkDiv');
+const metallicaDivNode = document.getElementById('metallicaDiv')
+
+pinkdivNode.addEventListener('click', () => {
+    searchAPI('pink floyd');
+})
+daftPunkDivNode.addEventListener('click', () => {
+    searchAPI('daft punk');
+})
+metallicaDivNode.addEventListener('click', () => {
+    searchAPI('metallica');
+})
+const searchAPI = (name) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7b30b9fc0fmsh3d27e8873d00596p1a601cjsn72842e9b2fab',
+            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+        }
+    };
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${name}`, options)
+        .then(response => response.json())
+        .then(response => {
+            firstDivNode.innerHTML = '';
+            const { data } = response;
+            console.log(data);
+            data.map((obj, i) => {
+                return firstDivNode.innerHTML +=
+                    `<div class="col-sm-6 col-md-4 col-xl-3 px-1 mb-3 mb-sm-2 mb-md-0 mb-xl-2">
+                        <div class="card" style="width: 18rem">
+                            <img src=${obj.artist.picture} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                            <h5 class="card-title">${obj.title_short}</h5>
+                            <p class="card-text">${obj.title}</p>
+                            <a href="#" class="btn btn-primary">${obj.rank}</a>
+                        </div>
+                    </div>`
+            })
+        })
+        .catch(error => {
+            console.error(error)
+            firstDivNode.innerHTML = `<div class="d-flex justify-content-center">
+                                        <span class="font-weight-bolder">Data Not Found</span>
+                                      </div>`
+        });
+}
+window.onload = () => {
+    firstDivNode.innerHTML = `
+    <div class="d-flex justify-content-center align-items-center">
+    <div class="d-flex spinner-border text-success" role="status">
+<span class="sr-only">Loading...</span>
+</div>
+</div>`
+    searchAPI('pink floyd');
+}
